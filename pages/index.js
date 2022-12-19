118c44cc80a99be9a51e03a44bdd66f6d1da064c
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2022-12-19 20:17:02 -0300

    contador

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
 function Home(){
-    return <div>Home</div>
+    return (
+        <div>
+            <h1>Home</h1>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador(){
+    const [contador, setContador] = useState(1);
+
+    function addCount(){setContador(contador+1)};
+
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={addCount}>Clica ai meu nobre</button>
+        </div>
+    )
 }
 
 export default Home
